1f766c8d035bb40f68ba45952f1e9d3a4c817652
shmin <gks831@naver.com>
2023-07-03 23:35:35 +0900

    Add Create Contents Function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-// ==================== ====================
+// ==================== ==================== ====================
 // Global Variable
-// ==================== ====================
+// ==================== ==================== ====================
 this.today = 'hello';
 
 
@@ -11,14 +11,50 @@ this.today = 'hello';
 // Document OnLoad Event.
 // ==================== ==================== ====================
 document.addEventListener("DOMContentLoaded", function(){
+    // =============== =============== ===============
+    // Create LocalStorage When Site Come First.
+    // =============== =============== ===============
     if (localStorage.getItem('contents') == null) {
         localStorage.setItem('contents', "[]");
+        localStorage.setItem('nextval', 0);
     }
 
+    // =============== =============== ===============
+    // Generate Content Tag.
+    // =============== =============== ===============
     const storage = JSON.parse(localStorage.getItem('contents'));
-    
-    
+    if (storage.length != 0) storage.forEach(content => {
+        const isExist = document.getElementById('content_id_' + content.id);
+        if (isExist != null) return;
 
+        let textContent = '';
+        textContent += '<div id="content_id_' + content.id +'">';
+        textContent +=    '<div class="Content-wrap">';
+        textContent +=        '<div class="Content-inner" style="width: 25%;">';
+        textContent +=            '<p>' + content.date + '</p>';
+        textContent +=        '</div>';
+        textContent +=        '<div class="Vertical-line"></div>';
+        textContent +=        '<div class="Content-inner" style="width: 50%;">';
+        textContent +=            '<p>' + content.title + '</p>';
+        textContent +=        '</div>';
+        textContent +=        '<div class="Vertical-line"></div>';
+        textContent +=        '<div class="Content-inner" style="width: 25%;">';
+        textContent +=            '<div class="Content-button" onclick="fnShowHideModal(\'content\', true, false, ' + content.id + ')">수정</div>';
+        textContent +=        '</div>';
+        textContent +=    '</div>';
+        textContent +=    '<div class="Bottom-line"></div>';
+        textContent += '</div>';
+
+        const template = document.createElement('template');
+        template.innerHTML = textContent;
+
+        const contentsBody = document.getElementById('contentsBody');
+        contentsBody.appendChild(template.content.firstChild);
+    });
+    
+    // =============== =============== ===============
+    // Set DatePicker First.
+    // =============== =============== ===============
     fnSetDate();
 });
 
@@ -56,7 +92,7 @@ const fnSetDate = () => {
 
 
 // ==================== ==================== ====================
-// Change Month Tag Event
+// Change Month Tag Event.
 // ==================== ==================== ====================
 const fnChangeMonth = (isChange) => {
     const Year = document.getElementById('dateY').value;
@@ -73,6 +109,26 @@ const fnChangeMonth = (isChange) => {
     }
 
     parentD.value = (isChange) ? 1 : new Date().getDate();
+    if (!isChange) {fnChangeDatepicker(new Date().toISOString().split('T')[0]);}
+}
+
+
+
+
+
+// ==================== ==================== ====================
+// Change Datepicker Value Evnet.
+// ==================== ==================== ====================
+const fnChangeDatepicker = () => {
+    let yy = document.getElementById('dateY').value;
+    let mm = document.getElementById('dateM').value;
+    let dd = document.getElementById('dateD').value;
+    mm = (mm.length == 1) ? '0' + mm : mm;
+    dd = (dd.length == 1) ? '0' + dd : dd;
+    
+    const date = yy + '-' + mm + '-' + dd;
+    document.getElementById('datepicker').value = date;
+    fnShowHideModal('date', false);
 }
 
 
@@ -82,12 +138,34 @@ const fnChangeMonth = (isChange) => {
 // ==================== ==================== ====================
 // Modal OFF Event
 // ==================== ==================== ====================
-const fnShowHideModal = (id, isBlock) => {
+const fnShowHideModal = (id, isBlock, isCreate, content_id) => {
+    // =============== =============== ===============
+    // Show Popup-Wrap or Date-Wrap.
+    // =============== =============== ===============
     if (id == 'content') id = 'Popup-wrap';
     if (id == 'date')    id = 'Date-wrap';
-    isBlock = isBlock ? 'flex' : 'none';
+    document.getElementsByClassName(id)[0].style.display = isBlock ? 'flex' : 'none';
 
-    document.getElementsByClassName(id)[0].style.display = isBlock;
+    // =============== =============== ===============
+    // Change Popup-Warp Footer (Create / Update)
+    // =============== =============== ===============
+    let createWrap = document.getElementById('CreateWrap');
+    let updateWrap = document.getElementById('UpdateWrap');
+    if (isCreate != null && isCreate == true)  { createWrap.style.display = 'flex'; updateWrap.style.display = 'none'; fnSetDate(); document.getElementById('title').value = '';}
+    if (isCreate != null && isCreate == false) { createWrap.style.display = 'none'; updateWrap.style.display = 'flex'; }
+
+    // =============== =============== ===============
+    // Change Content Value.
+    // =============== =============== ===============
+    if (content_id != null) {
+        let contents = JSON.parse(localStorage.getItem('contents'));
+        if (contents != null) contents.forEach(content => {
+            if (content.id == content_id) {
+                document.getElementById('title').value = content.title;
+                document.getElementById('datepicker').value = content.date;
+            }
+        });
+    }
 }
 
 
@@ -97,7 +175,19 @@ const fnShowHideModal = (id, isBlock) => {
 // ==================== ==================== ====================
 // Modal ON Event
 // ==================== ==================== ====================
-const fnOpenModal = (id, value) => {
-    if (id == 'date') id = 'Date-wrap';
+const fnCreateContent = () => {
+    const content = {
+        id:localStorage.getItem('nextval'),
+        title:document.getElementById('title').value,
+        date:document.getElementById('datepicker').value
+    };
 
+    const storage = JSON.parse(localStorage.getItem('contents'));
+    storage.push(content);
+
+    const newStorage = JSON.stringify(storage);
+    localStorage.setItem('contents', newStorage);
+
+    fnShowHideModal('content', false);
+    window.location.reload();
 }
